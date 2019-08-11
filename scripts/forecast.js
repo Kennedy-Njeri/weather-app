2c9d215e0e59882fe2c24becc461ac8378ffe972
Kennedy Njeri <mistakenz123@gmail.com>
2019-08-11 13:01:05 +0300

    getting weather information

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,32 @@
 const key = 'uyMxjkG2IdEv4VBCLkYcvvgGqBkK6XkU'
 
 
+// get weather information
+
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+
+    const query = `${id}?apikey=${key}`
+
+    const response = await fetch(base + query)
+
+    if(response.status !==200){
+        throw new Error("cannot fetch the data")
+    }
+
+
+    const data = await response.json()
+
+    //return data[0]
+    console.log(data)
+
+}
+
+
+
+
+// get city weather
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
@@ -19,7 +45,18 @@ const getCity = async (city) => {
 
 
 getCity('nairobi').then(data => {
+    return getWeather(data.Key)
+    //console.log(data)
+}).then(data => {
     console.log(data)
 }).catch(err => {
-    console.log(err)
+    console.log(err.message)
 })
+
+// getCity("Nairobi").then(data => {
+//     console.log(data)
+// }).catch(err => {
+//     console.log(err)
+// })
+
+//getWeather("224758")
